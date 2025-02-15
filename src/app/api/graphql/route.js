9bd8371c5718e1f0dea17e0d6570b4ab15b6bf16
Tanishq Gupta <tanishq@hipster-dev.com>
2025-02-15 15:59:20 +0530

    graphql fix

---
+++ src/app/api/graphql/route.js
@@ -0,0 +1,91 @@
+import { NextResponse } from "next/server";
+import { GraphQLBigInt } from "graphql-scalars";
+// import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer } from "@apollo/server";
+import { PrismaClient } from "@prisma/client";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+
+
+const prisma = new PrismaClient();
+
+// Define GraphQL schema
+const typeDefs = `
+ scalar BigInt  # ✅ Define BigInt scalar
+  type User {
+    id: BigInt!
+    name: String!
+    email: String!
+  }
+  
+ type Query {
+    users: [User]
+    user(id: ID!): User  
+  }
+
+  type Role {
+    id: BigInt! 
+    name: String!
+  }
+  
+ type Query {
+    roles: [Role]
+    role(id: ID!): Role  
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+  }
+`;
+
+// Define resolvers
+// GraphQL Resolvers using Prisma
+const resolvers = {
+    BigInt: GraphQLBigInt,  // ✅ Add support for BigInt
+    Query: {
+      users: async () => await prisma.user.findMany(),
+      user: async (_, { id }) => await prisma.user.findUnique({ where: { id: Number(id) } }),
+      roles: async () => {
+        return await prisma.role.findMany();
+      },
+      role: async (_, { id }) => {
+        return await prisma.role.findUnique({ where: { id: Number(id) } });
+      },
+    },
+    // Mutation: {
+    //   createUser: async (_, { name, email }) => {
+    //     return await prisma.user.create({ data: { name, email } });
+    //   },
+    //   deleteUser: async (_, { id }) => {
+    //     return await prisma.user.delete({ where: { id: Number(id) } });
+    //   },
+    // },
+  };
+  
+  // Create Apollo Server
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const apolloServer = new ApolloServer({ schema });
+  
+  export const config = {
+    api: {
+      bodyParser: false,
+    },
+  };
+
+  
+//   export default async function handler(req, res) {
+//       await apolloServer.start();
+//       return apolloServer.createHandler({ path: "/graphql" })(req, res);
+//     }
+    
+
+  const handler = startServerAndCreateNextHandler(apolloServer);
+
+  export async function GET(request) {
+    return handler(request);
+  }
+  
+  export async function POST(request) {
+    return handler(request);
+  }
