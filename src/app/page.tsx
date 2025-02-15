5eb608afb52eb9a6edbec90288127412d21bcee0
Tanishq Gupta <tanishq@hipster-dev.com>
2025-02-15 14:50:58 +0530

    graphql npm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Footer from "@/compoments/Footer";
+
+import Navbar from "@/compoments/Navbar";
 
 export default function Home() {
   return (
  <>
- 
+       <Navbar/>
+       <Footer/>
  </>
   );
 }
