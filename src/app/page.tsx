fc8cd5fc6c71abdeb743b64c4e875ef00e476415
Tanishq Gupta <tanishqhupta18=9@gmail.com>
2025-02-20 22:50:40 +0530

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-"use client";
 import Footer_v1 from "@/compoments/footer/Footer_v1";
 import Navbar_v1 from "@/compoments/navbar/Navbar_v1";
 import CustomHead from "./CustomHead";
-import useGetDmain from "../Hook/useGetDmain";
+
 
 // import { gql, useQuery } from '@apollo/client';
 
-// interface roles {
-//   id: string;
-//   name: string;
-// }
-
-// interface GetDataResponse {
-//   roles: roles[];
-// }
+// import {GetDataResponse} from "../interface/types";
 
 // const GET_DATA = gql`
 // query RoleData {
@@ -26,7 +18,7 @@ import useGetDmain from "../Hook/useGetDmain";
 
 export default function Home() {
   // const { loading, error, data } = useQuery<GetDataResponse>(GET_DATA);
-  const daa = useGetDmain()
+  
   // if (loading) return <p>Loading...</p>;
   // if (error) return <p>Error fkc: {error.message}</p>;
   
@@ -36,13 +28,6 @@ export default function Home() {
       {/* {getDmain()} */}
       <CustomHead />
       <Navbar_v1 />
-
-
-          {/* <ul>
-          {data.roles.map((user) => (
-            <li key={user.id}>{user.name}</li> // ✅ Render names in a list
-          ))}
-        </ul> */}
       
       <Footer_v1 />
     </>
