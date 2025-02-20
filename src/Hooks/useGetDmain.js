6d2efd9520b3603f347221fc6fe59530c127e46e
Tanishq Gupta <tanishqhupta18=9@gmail.com>
2025-02-20 22:53:58 +0530

    pahe

---
+++ src/Hooks/useGetDmain.js
@@ -0,0 +1,28 @@
+"use client"
+import { useState, useEffect } from 'react';
+
+const GetDomain = (url) => {
+    // 
+    console.log(url);
+    
+    // const [serverUrl, setServerUrl] = useState(url);
+    const [serverUrl, setServerUrl] = useState('https://food.localhost:1234/event/73');
+
+    
+
+    useEffect(() => {
+        const url = new URL(serverUrl);
+
+        const hostParts = url.hostname.split('.');
+        const subdomain = hostParts.length > 2 ? hostParts[0] : null;
+    
+
+
+
+        if (subdomain) localStorage.setItem('subdomain', subdomain);
+      
+ 
+    }, [serverUrl]);
+
+}
+export default GetDomain
