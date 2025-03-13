6a6a4b3b41fdfb5b632eaefa6ad5f74abaa31ffe
Tanishq Gupta <tanishqhupta18=9@gmail.com>
2025-03-13 06:39:09 +0530

    amedn

---
+++ src/app/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+  }
+
+  h1 {
+    color: #333;
+  }
+
+  p {
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyle;
