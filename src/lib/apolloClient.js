d27dfeb109b7b0e5a2ec68467eefee1fec769109
Tanishq Gupta <tanishqhupta18=9@gmail.com>
2025-03-16 16:36:52 +0530

    domain remove

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,29 +1,34 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+// import { setContext } from "@apollo/client/link/context";
 
-import { onError } from "@apollo/client/link/error";
-import { ApolloLink } from "@apollo/client";
+// import { onError } from "@apollo/client/link/error";
+// import { ApolloLink } from "@apollo/client";
 const httpLink = createHttpLink({
   uri: "/api/graphql",
 });
 
-const authLink = setContext((_, { headers }) => {
-  // Retrieve the token from localStorage, sessionStorage, or cookies
+// const authLink = setContext((_, { headers }) => {
+//   // Retrieve the token from localStorage, sessionStorage, or cookies
 
-  const sub_domain = localStorage.getItem('sub_domain');
+//   const sub_domain = localStorage.getItem('sub_domain');
 
-  return {
-    headers: {
-      ...headers,
+//   return {
+//     headers: {
+//       ...headers,
 
-      sub_domain: sub_domain ? String(sub_domain) : "",
-    },
-  };
-});
+//       sub_domain: sub_domain ? String(sub_domain) : "",
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
 
 export default client;
