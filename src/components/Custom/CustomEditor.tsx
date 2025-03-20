9a57a349ec03baa3bdf0d8064c2be70ad63e6448
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-20 18:53:50 +0530

    fix

--- src/components/Custom/CustomEditor.tsx
+++ src/components/Custom/CustomEditor.tsx
@@ -2,27 +2,33 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { ClassicEditor, Essentials, Paragraph, Bold, Italic } from 'ckeditor5';
 import { FormatPainter } from 'ckeditor5-premium-features';
-
+import React, { useState, useEffect } from "react";validateHeaderValue
 import 'ckeditor5/ckeditor5.css';
 import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
+import { validateHeaderValue } from 'node:http';
 
 function CustomEditor() {
     console.log(`${process.env.Editer_URL_trail}`);
-    
+      const [text, setValue] = useState("");
     return (
         <CKEditor
             editor={ ClassicEditor }
             config={ {
-                licenseKey: `${process.env.Editer_URL}`, // Or 'GPL'.
+                licenseKey: `${process.env.REACT_APP_EDITOR_URL}`, // Or 'GPL'.
                 plugins: [ Essentials, Paragraph, Bold, Italic, FormatPainter ],
                 toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', '|', 'formatPainter' ],
                 // initialData: '<p>Hello from CKEditor 5 in React!</p>',
             } }
-            data='<p>Hello from the second editor working with the context!</p>'
+            data={text}
             onReady={ ( editor ) => {
                 // You can store the "editor" and use when it is needed.
                 console.log( 'Editor 2 is ready to use!', editor );
               } }
+            onChange={ (event , editor) => {
+                const data = editor.getData();
+                setValue(data)
+                
+            } }
         />
     );
 }
