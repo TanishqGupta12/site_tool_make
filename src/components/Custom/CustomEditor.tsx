208eadb1d4facd37fe9dc6bba08651f6947280f7
Tanishq Gupta <tanishqhupta18=9@gmail.com>
2025-03-22 18:38:28 +0530

    css

--- src/components/Custom/CustomEditor.tsx
+++ src/components/Custom/CustomEditor.tsx
@@ -3,308 +3,420 @@ import { CustomInputProps } from "@premieroctet/next-admin";
 import { useState, useEffect, useRef, useMemo } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import {
-  DecoupledEditor,
-  Alignment,
-  AutoImage,
-  AutoLink,
-  Autosave,
-  BalloonToolbar,
-  BlockToolbar,
-  Bold,
-  Bookmark,
-  Code,
-  Essentials,
-  FontBackgroundColor,
-  FontColor,
-  FontFamily,
-  FontSize,
-  GeneralHtmlSupport,
-  Heading,
-  HorizontalLine,
-  HtmlComment,
-  HtmlEmbed,
-  ImageBlock,
-  ImageCaption,
-  ImageEditing,
-  ImageInline,
-  ImageInsert,
-  ImageInsertViaUrl,
-  ImageResize,
-  ImageStyle,
-  ImageTextAlternative,
-  ImageToolbar,
-  ImageUpload,
-  ImageUtils,
-  Indent,
-  IndentBlock,
-  Italic,
-  Link,
-  LinkImage,
-  List,
-  ListProperties,
-  Markdown,
-  MediaEmbed,
-  Mention,
-  PageBreak,
-  Paragraph,
-  PasteFromMarkdownExperimental,
-  PasteFromOffice,
-  RemoveFormat,
-  ShowBlocks,
-  SimpleUploadAdapter,
-  Strikethrough,
-  Subscript,
-  Superscript,
-  Table,
-  TableCaption,
-  TableCellProperties,
-  TableColumnResize,
-  TableProperties,
-  TableToolbar,
-  TextPartLanguage,
-  Title,
-  TodoList,
-  Underline,
-  WordCount
+	DecoupledEditor,
+	Alignment,
+	Autoformat,
+	AutoImage,
+	AutoLink,
+	Autosave,
+	BalloonToolbar,
+	Base64UploadAdapter,
+	BlockQuote,
+	Bold,
+	Bookmark,
+	Code,
+	CodeBlock,
+	Emoji,
+	Essentials,
+	FindAndReplace,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	GeneralHtmlSupport,
+	Heading,
+	Highlight,
+	HorizontalLine,
+	HtmlComment,
+	HtmlEmbed,
+	ImageBlock,
+	ImageCaption,
+	ImageEditing,
+	ImageInline,
+	ImageInsert,
+	ImageInsertViaUrl,
+	ImageResize,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageToolbar,
+	ImageUpload,
+	ImageUtils,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	LinkImage,
+	List,
+	ListProperties,
+	Markdown,
+	MediaEmbed,
+	Mention,
+	Paragraph,
+	PasteFromMarkdownExperimental,
+	PasteFromOffice,
+	RemoveFormat,
+	ShowBlocks,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
+	Style,
+	Subscript,
+	Superscript,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	TextPartLanguage,
+	TextTransformation,
+	Title,
+	TodoList,
+	Underline,
+	WordCount
 } from 'ckeditor5';
 
+// Define the Props type, extending CustomInputProps
+type Props = CustomInputProps;
+
 import 'ckeditor5/ckeditor5.css';
+
 import './CustomEditor.css';
 
 const LICENSE_KEY =
-  'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDM4OTc1OTksImp0aSI6IjMxNDhjMTY2LWFhNzEtNDU5NS04OGM2LTQ3MTJlZjNhNTZmMyIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjU5ODBjYTA2In0.hIAbFg3WHszuYAhtBRH4WrMF23fBEevLMIXP95zgdhlFv1r2ECK771jWtywvQy-hiZoGT7tEGUL8iDEqJCL6mg';
+	'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDM4OTc1OTksImp0aSI6IjMxNDhjMTY2LWFhNzEtNDU5NS04OGM2LTQ3MTJlZjNhNTZmMyIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjU5ODBjYTA2In0.hIAbFg3WHszuYAhtBRH4WrMF23fBEevLMIXP95zgdhlFv1r2ECK771jWtywvQy-hiZoGT7tEGUL8iDEqJCL6mg';
+  const CustomEditor = ({ value, name, onChange, disabled, required }: Props) => {
+	const editorContainerRef = useRef(null);
+	const editorMenuBarRef = useRef(null);
+	const editorToolbarRef = useRef(null);
+	const editorRef = useRef(null);
+	const editorWordCountRef = useRef(null);
+	const [isLayoutReady, setIsLayoutReady] = useState(false);
 
-type Props = CustomInputProps;
-
-
-const CustomEditor = ({ value, name, onChange, disabled, required }: Props) => {
-  const editorContainerRef = useRef(null);
-  const editorMenuBarRef = useRef(null);
-  const editorToolbarRef = useRef(null);
-  const editorRef = useRef(null);
-  const editorWordCountRef = useRef(null);
-  const [isLayoutReady, setIsLayoutReady] = useState(false);
-	console.log(value);
-	
   const [dataText, setDataText] = useState<string>(value || "");
 
-  useEffect(() => {
-    setIsLayoutReady(true);
-    return () => setIsLayoutReady(false);
-  }, []);
+	useEffect(() => {
+		setIsLayoutReady(true);
 
-  const { editorConfig } = useMemo(() => {
-    if (!isLayoutReady) {
-      return {};
-    }
+		return () => setIsLayoutReady(false);
+	}, []);
 
-    return {
-      editorConfig: {
-        toolbar: {
-          items: [
-            'showBlocks',
-            '|',
-            'heading',
-            '|',
-            'fontSize',
-            'fontFamily',
-            'fontColor',
-            'fontBackgroundColor',
-            '|',
-            'bold',
-            'italic',
-            'underline',
-            '|',
-            'link',
-            'insertImage',
-            'insertTable',
-            '|',
-            'alignment',
-            '|',
-            'bulletedList',
-            'numberedList',
-            'todoList',
-            'outdent',
-            'indent'
-          ],
-          shouldNotGroupWhenFull: false
-        },
-        plugins: [
-          Alignment,
-          AutoImage,
-          AutoLink,
-          Autosave,
-          BalloonToolbar,
-          BlockToolbar,
-          Bold,
-          Bookmark,
-          Code,
-          Essentials,
-          FontBackgroundColor,
-          FontColor,
-          FontFamily,
-          FontSize,
-          GeneralHtmlSupport,
-          Heading,
-          HorizontalLine,
-          HtmlComment,
-          HtmlEmbed,
-          ImageBlock,
-          ImageCaption,
-          ImageEditing,
-          ImageInline,
-          ImageInsert,
-          ImageInsertViaUrl,
-          ImageResize,
-          ImageStyle,
-          ImageTextAlternative,
-          ImageToolbar,
-          ImageUpload,
-          ImageUtils,
-          Indent,
-          IndentBlock,
-          Italic,
-          Link,
-          LinkImage,
-          List,
-          ListProperties,
-          Markdown,
-          MediaEmbed,
-          Mention,
-          PageBreak,
-          Paragraph,
-          PasteFromMarkdownExperimental,
-          PasteFromOffice,
-          RemoveFormat,
-          ShowBlocks,
-          SimpleUploadAdapter,
-          Strikethrough,
-          Subscript,
-          Superscript,
-          Table,
-          TableCaption,
-          TableCellProperties,
-          TableColumnResize,
-          TableProperties,
-          TableToolbar,
-          TextPartLanguage,
-          Title,
-          TodoList,
-          Underline,
-          WordCount
-        ],
-        balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
-        blockToolbar: [
-          'fontSize',
-          'fontColor',
-          'fontBackgroundColor',
-          '|',
-          'bold',
-          'italic',
-          '|',
-          'link',
-          'insertImage',
-          'insertTable',
-          '|',
-          'bulletedList',
-          'numberedList',
-          'outdent',
-          'indent'
-        ],
-        fontFamily: {
-          supportAllValues: true
-        },
-        fontSize: {
-          options: [10, 12, 14, 'default', 18, 20, 22],
-          supportAllValues: true
-        },
-        heading: {
-          options: [
-            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
-            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
-            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
-          ]
-        },
-        htmlSupport: {
-          allow: [
-            {
-              name: /^.*$/,
-              styles: true,
-              attributes: true,
-              classes: true
-            }
-          ]
-        },
-        image: {
-          toolbar: [
-            'toggleImageCaption',
-            'imageTextAlternative',
-            '|',
-            'imageStyle:inline',
-            'imageStyle:wrapText',
-            'imageStyle:breakText',
-            '|',
-            'resizeImage'
-          ]
-        },
-        licenseKey: LICENSE_KEY,
-        link: {
-          addTargetToExternalLinks: true,
-          defaultProtocol: 'https://',
-          decorators: {
-            toggleDownloadable: {
-              mode: 'manual',
-              label: 'Downloadable',
-              attributes: { download: 'file' }
-            }
-          }
-        },
-        list: {
-          properties: {
-            styles: true,
-            startIndex: true,
-            reversed: true
-          }
-        },
-        mention: { feeds: [{ marker: '@', feed: [] }] },
-        menuBar: { isVisible: true },
-        placeholder: 'Type or paste your content here!',
-        table: {
-          contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-        }
-      }
-    };
-  }, [isLayoutReady]);
+	const { editorConfig } = useMemo(() => {
+		if (!isLayoutReady) {
+			return {};
+		}
 
-  return (
-    <div className="main-container">
-      {editorConfig ? (
-        <div className="editor-container editor-container_document-editor editor-container_include-word-count" ref={editorContainerRef}>
-          <div className="editor-container__menu-bar" ref={editorMenuBarRef}></div>
-          <div className="editor-container__toolbar" ref={editorToolbarRef}></div>
-          <div className="editor-container__editor-wrapper">
-            <div className="editor-container__editor">
-              <div ref={editorRef}>
-                <CKEditor
+		return {
+			editorConfig: {
+				toolbar: {
+					items: [
+						'showBlocks',
+						'|',
+						'heading',
+						'style',
+						'|',
+						'fontSize',
+						'fontFamily',
+						'fontColor',
+						'fontBackgroundColor',
+						'|',
+						'bold',
+						'italic',
+						'underline',
+						'|',
+						'link',
+						'insertImage',
+						'insertTable',
+						'highlight',
+						'blockQuote',
+						'codeBlock',
+						'|',
+						'alignment',
+						'|',
+						'bulletedList',
+						'numberedList',
+						'todoList',
+						'outdent',
+						'indent'
+					],
+					shouldNotGroupWhenFull: false
+				},
+				plugins: [
+					Alignment,
+					Autoformat,
+					AutoImage,
+					AutoLink,
+					Autosave,
+					BalloonToolbar,
+					Base64UploadAdapter,
+					BlockQuote,
+					Bold,
+					Bookmark,
+					Code,
+					CodeBlock,
+					Emoji,
+					Essentials,
+					FindAndReplace,
+					FontBackgroundColor,
+					FontColor,
+					FontFamily,
+					FontSize,
+					GeneralHtmlSupport,
+					Heading,
+					Highlight,
+					HorizontalLine,
+					HtmlComment,
+					HtmlEmbed,
+					ImageBlock,
+					ImageCaption,
+					ImageEditing,
+					ImageInline,
+					ImageInsert,
+					ImageInsertViaUrl,
+					ImageResize,
+					ImageStyle,
+					ImageTextAlternative,
+					ImageToolbar,
+					ImageUpload,
+					ImageUtils,
+					Indent,
+					IndentBlock,
+					Italic,
+					Link,
+					LinkImage,
+					List,
+					ListProperties,
+					Markdown,
+					MediaEmbed,
+					Mention,
+					Paragraph,
+					PasteFromMarkdownExperimental,
+					PasteFromOffice,
+					RemoveFormat,
+					ShowBlocks,
+					SpecialCharacters,
+					SpecialCharactersArrows,
+					SpecialCharactersCurrency,
+					SpecialCharactersEssentials,
+					SpecialCharactersLatin,
+					SpecialCharactersMathematical,
+					SpecialCharactersText,
+					Strikethrough,
+					Style,
+					Subscript,
+					Superscript,
+					Table,
+					TableCaption,
+					TableCellProperties,
+					TableColumnResize,
+					TableProperties,
+					TableToolbar,
+					TextPartLanguage,
+					TextTransformation,
+					Title,
+					TodoList,
+					Underline,
+					WordCount
+				],
+				balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
+				fontFamily: {
+					supportAllValues: true
+				},
+				fontSize: {
+					options: [10, 12, 14, 'default', 18, 20, 22],
+					supportAllValues: true
+				},
+				heading: {
+					options: [
+						{
+							model: 'paragraph',
+							title: 'Paragraph',
+							class: 'ck-heading_paragraph'
+						},
+						{
+							model: 'heading1',
+							view: 'h1',
+							title: 'Heading 1',
+							class: 'ck-heading_heading1'
+						},
+						{
+							model: 'heading2',
+							view: 'h2',
+							title: 'Heading 2',
+							class: 'ck-heading_heading2'
+						},
+						{
+							model: 'heading3',
+							view: 'h3',
+							title: 'Heading 3',
+							class: 'ck-heading_heading3'
+						},
+						{
+							model: 'heading4',
+							view: 'h4',
+							title: 'Heading 4',
+							class: 'ck-heading_heading4'
+						},
+						{
+							model: 'heading5',
+							view: 'h5',
+							title: 'Heading 5',
+							class: 'ck-heading_heading5'
+						},
+						{
+							model: 'heading6',
+							view: 'h6',
+							title: 'Heading 6',
+							class: 'ck-heading_heading6'
+						}
+					]
+				},
+				htmlSupport: {
+					allow: [
+						{
+							name: /^.*$/,
+							styles: true,
+							attributes: true,
+							classes: true
+						}
+					]
+				},
+				image: {
+					toolbar: [
+						'toggleImageCaption',
+						'imageTextAlternative',
+						'|',
+						'imageStyle:inline',
+						'imageStyle:wrapText',
+						'imageStyle:breakText',
+						'|',
+						'resizeImage'
+					]
+				},
+				licenseKey: LICENSE_KEY,
+				link: {
+					addTargetToExternalLinks: true,
+					defaultProtocol: 'https://',
+					decorators: {
+						toggleDownloadable: {
+							mode: 'manual',
+							label: 'Downloadable',
+							attributes: {
+								download: 'file'
+							}
+						}
+					}
+				},
+				list: {
+					properties: {
+						styles: true,
+						startIndex: true,
+						reversed: true
+					}
+				},
+				mention: {
+					feeds: [
+						{
+							marker: '@',
+							feed: [
+								/* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+							]
+						}
+					]
+				},
+				menuBar: {
+					isVisible: true
+				},
+				placeholder: 'Type or paste your content here!',
+				style: {
+					definitions: [
+						{
+							name: 'Article category',
+							element: 'h3',
+							classes: ['category']
+						},
+						{
+							name: 'Title',
+							element: 'h2',
+							classes: ['document-title']
+						},
+						{
+							name: 'Subtitle',
+							element: 'h3',
+							classes: ['document-subtitle']
+						},
+						{
+							name: 'Info box',
+							element: 'p',
+							classes: ['info-box']
+						},
+						{
+							name: 'Side quote',
+							element: 'blockquote',
+							classes: ['side-quote']
+						},
+						{
+							name: 'Marker',
+							element: 'span',
+							classes: ['marker']
+						},
+						{
+							name: 'Spoiler',
+							element: 'span',
+							classes: ['spoiler']
+						},
+						{
+							name: 'Code (dark)',
+							element: 'pre',
+							classes: ['fancy-code', 'fancy-code-dark']
+						},
+						{
+							name: 'Code (bright)',
+							element: 'pre',
+							classes: ['fancy-code', 'fancy-code-bright']
+						}
+					]
+				},
+				table: {
+					contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+				}
+			}
+		};
+	}, [isLayoutReady]);
+
+	return (
+		<div className="main-container">
+			<div
+				className="editor-container editor-container_document-editor editor-container_include-style editor-container_include-word-count"
+				ref={editorContainerRef}
+			>
+				<div className="editor-container__menu-bar" ref={editorMenuBarRef}></div>
+				<div className="editor-container__toolbar" ref={editorToolbarRef}></div>
+				<div className="editor-container__editor-wrapper">
+					<div className="editor-container__editor">
+						<div ref={editorRef}>
+							{editorConfig && (
+								<CKEditor
+                  data={dataText}
                   name={name}
-                  // onAfterDestroy={() => {
-                  //   Array.from(editorWordCountRef.current.children).forEach(child => child.remove());
-                  //   Array.from(editorToolbarRef.current.children).forEach(child => child.remove());
-                  //   Array.from(editorMenuBarRef.current.children).forEach(child => child.remove());
-                  // }}
-                  onReady={editor => {
-                    const wordCount = editor.plugins.get('WordCount');
-                    editorWordCountRef.current.appendChild(wordCount.wordCountContainer);
-                    editorToolbarRef.current.appendChild(editor.ui.view.toolbar.element);
-                    editorMenuBarRef.current.appendChild(editor.ui.view.menuBarView.element);
-                  }}
-					        data={dataText}
-                  editor={DecoupledEditor}
-                  config={editorConfig}
+									onReady={editor => {
+										const wordCount = editor.plugins.get('WordCount');
+										editorWordCountRef.current.appendChild(wordCount.wordCountContainer);
+										editorToolbarRef.current.appendChild(editor.ui.view.toolbar.element);
+										editorMenuBarRef.current.appendChild(editor.ui.view.menuBarView.element);
+									}}
+									onAfterDestroy={() => {
+										Array.from(editorWordCountRef.current.children).forEach(child => child.remove());
+										Array.from(editorToolbarRef.current.children).forEach(child => child.remove());
+										Array.from(editorMenuBarRef.current.children).forEach(child => child.remove());
+									}}
+									editor={DecoupledEditor}
+									config={editorConfig}
                   onChange={(event, editor) => {
                     const newData = editor.getData();
                     setDataText(newData);
@@ -313,17 +425,14 @@ const CustomEditor = ({ value, name, onChange, disabled, required }: Props) => {
                       target: { value: newData},
                     })
                   }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="editor_container__word-count" ref={editorWordCountRef}></div>
-        </div>
-      ) : (
-        <div>Loading editor...</div>
-      )}
-    </div>
-  );
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+				<div className="editor_container__word-count" ref={editorWordCountRef}></div>
+			</div>
+		</div>
+	);
 }
-
-export default CustomEditor;
+export default CustomEditor ;
