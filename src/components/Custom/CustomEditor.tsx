62a69fd881ef2364c1b81864bb71864be14e2640
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-22 07:24:27 +0530

    editor

--- src/components/Custom/CustomEditor.tsx
+++ src/components/Custom/CustomEditor.tsx
@@ -1,11 +1,10 @@
 "use client"
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { ClassicEditor, Essentials, Paragraph, Bold, Italic } from 'ckeditor5';
-import { FormatPainter } from 'ckeditor5-premium-features';
-import React, { useState, useEffect } from "react";validateHeaderValue
+
+import React, { useState } from "react";
 import 'ckeditor5/ckeditor5.css';
-import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
-import { validateHeaderValue } from 'node:http';
+
 
 function CustomEditor() {
     console.log(`${process.env.Editer_URL_trail}`);
@@ -14,15 +13,14 @@ function CustomEditor() {
         <CKEditor
             editor={ ClassicEditor }
             config={ {
-                licenseKey: `${process.env.REACT_APP_EDITOR_URL}`, // Or 'GPL'.
-                plugins: [ Essentials, Paragraph, Bold, Italic, FormatPainter ],
-                toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', '|', 'formatPainter' ],
+                licenseKey: `eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDM1NTE5OTksImp0aSI6IjJlZTc4NDRkLWU1N2ItNDFjMC1hN2E1LTIzYWFjZDcwNmY1OSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImIyZWM3ZmMzIn0.NP5or-UZG6xxJCnxsZq2DzCSLL7D-LWAeMofpkaf4zWMKtRhZT4YzzBnaWFc7A2OPUE0c9RtRCeIhoO3m-EkAA`, // Or 'GPL'.
+                plugins: [ Essentials, Paragraph, Bold, Italic,  ],
+                toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', '|' ],
                 // initialData: '<p>Hello from CKEditor 5 in React!</p>',
             } }
             data={text}
             onReady={ ( editor ) => {
-                // You can store the "editor" and use when it is needed.
-                console.log( 'Editor 2 is ready to use!', editor );
+   
               } }
             onChange={ (event , editor) => {
                 const data = editor.getData();
